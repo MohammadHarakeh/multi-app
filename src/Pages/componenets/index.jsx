43ae3bd02330ffebbf8f1d009187f3b63e9bd2b4
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-26 23:28:20 +0200

    initializing the project and folders

---
+++ src/Pages/componenets/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./Weather/weather";
+
+function MainPage() {
+  return (
+    <div className="main-wrapper">
+      <h1>test</h1>
+      <div>
+        <Weather></Weather>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
