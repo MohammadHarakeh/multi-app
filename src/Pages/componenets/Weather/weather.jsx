661233f6a46800dbbe55aec4ca68f83123c4d69e
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-27 22:00:23 +0200

    Added second API for the upcomming weather

--- src/Pages/componenets/Weather/weather.jsx
+++ src/Pages/componenets/Weather/weather.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./weather.css";
 
 const api = {
   key: "cbf8fcf84e8dd5d943bd8fdf313f859f",
@@ -8,18 +9,49 @@ const api = {
 function Weather() {
   const [search, setSearch] = useState("");
   const [weather, setWeather] = useState({});
+  const [predicted, setPredicted] = useState("");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
 
-  const searchPressed = () => {
-    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-      .then((result) => result.json())
-      .then((result) => {
-        setWeather(result);
-        console.log(result);
-      });
+  const searchPressed = async () => {
+    const response = await fetch(
+      `${api.base}weather?q=${search}&units=metric&APPID=${api.key}`
+    );
+    const data = await response.json();
+    setWeather(data);
+    setLat(data.coord.lat);
+    setLon(data.coord.lon);
+    console.log(data);
+    predictedWeather();
   };
+
+  const predictedWeather = async () => {
+    try {
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&timezone=auto&forecast_days=3`
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      setPredicted(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (lat !== "" && lon !== "") {
+      predictedWeather();
+    }
+  }, [lat, lon]);
+
   return (
-    <div>
-      <div>
+    <div className="weather-wrapper">
+      <div className="weather-card-wrapper">
         <h1>Weather App</h1>
 
         <div>
@@ -38,7 +70,12 @@ function Weather() {
 
         {weather.main && <p>{weather.main.temp}°C</p>}
 
-        <p>Sunny</p>
+        <p>{weather.main && weather.weather[0].main}</p>
+        {weather.main && weather.weather[0].description && (
+          <p>({weather.weather[0].description})</p>
+        )}
+
+        <p>{predicted.hourly && predicted.hourly.temperature_2m[67]}</p>
       </div>
     </div>
   );
