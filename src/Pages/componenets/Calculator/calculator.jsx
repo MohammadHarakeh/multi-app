175b9f2835dab497fb1df743953bb6ee8565f7b3
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-28 00:47:23 +0200

    added calculator buttons

--- src/Pages/componenets/Calculator/calculator.jsx
+++ src/Pages/componenets/Calculator/calculator.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./calculator.css";
 
 function Calculator() {
-  return <div>Calculator</div>;
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+
+  const navigate = useNavigate();
+  const backClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="calculator-wrapper">
+      <div>
+        <div className="calculator">
+          <div className="display">
+            <span>0</span>
+          </div>
+
+          <div className="operators">
+            <button>/</button>
+            <button>*</button>
+            <button>+</button>
+            <button>-</button>
+
+            <button>DEL</button>
+          </div>
+
+          <div className="digits">
+            {createDigits()}
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
+        </div>
+
+        <div className="search-wrapper back-btn">
+          <button onClick={backClick}>Back</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
