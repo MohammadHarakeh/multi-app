8b9bb96c87afad523f1e09efd6ce95be691eeab4
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-29 01:44:26 +0200

    finished notes

--- src/Pages/componenets/Notes/notes.jsx
+++ src/Pages/componenets/Notes/notes.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Draggable from "react-draggable";
+import { useNavigate } from "react-router-dom";
 import "./notes.css";
 
 function Notes() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const storedNotes = localStorage.getItem("notes");
+    return storedNotes ? JSON.parse(storedNotes) : [];
+  });
 
   const randomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
@@ -12,9 +16,10 @@ function Notes() {
   const addNote = () => {
     const newNote = {
       id: Math.random(),
-      x: 20,
-      y: 20,
+      x: 0,
+      y: 60,
       backgroundColor: randomColor(),
+      text: "",
     };
     setNotes([...notes, newNote]);
   };
@@ -23,7 +28,7 @@ function Notes() {
     setNotes(notes.filter((note) => note.id !== id));
   };
 
-  const handleDrag = (id, e, ui) => {
+  const handleDrag = (id, ui) => {
     setNotes(
       notes.map((note) =>
         note.id === id ? { ...note, x: ui.x, y: ui.y } : note
@@ -31,26 +36,49 @@ function Notes() {
     );
   };
 
+  const handleTextChange = (id, e) => {
+    const newText = e.target.value;
+    setNotes(
+      notes.map((note) => (note.id === id ? { ...note, text: newText } : note))
+    );
+  };
+
+  const navigate = useNavigate();
+  const backClick = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
   return (
-    <div className="notes-wrapper">
+    <div className="notes-wrapper search-wrapper">
       <button onClick={addNote}>Add Note</button>
       {notes.map((note) => (
         <Draggable
           key={note.id}
           defaultPosition={{ x: note.x, y: note.y }}
-          onStop={(e, ui) => handleDrag(note.id, e, ui)}
+          onStop={(e, ui) => handleDrag(note.id, ui)}
         >
           <div
             className="note"
             style={{ backgroundColor: note.backgroundColor }}
           >
-            <textarea></textarea>
-            <button className="back-calc" onClick={() => deleteNote(note.id)}>
+            <textarea
+              value={note.text}
+              onChange={(e) => handleTextChange(note.id, e)}
+            ></textarea>
+            <button className="delete-btn" onClick={() => deleteNote(note.id)}>
               Delete
             </button>
           </div>
         </Draggable>
       ))}
+
+      <div className="back-btn search-wrapper">
+        <button onClick={backClick}>Back</button>
+      </div>
     </div>
   );
 }
